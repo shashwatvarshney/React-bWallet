f2591e9986930cbd7f138ef84d82c7163ce15b1e
Shashwat Varshney <shashwat.varshney@comviva.com>
2020-04-07 18:28:15 +0530

    ninth commit

--- android/files/SetPin.js
+++ android/files/SetPin.js
@@ -15,22 +15,44 @@ export default class Def extends React.Component {
       }
  }
  handleNext=()=>{
-  var pin='Enter enter your new PIN!'
-  var cpin='Please re-enter your new PIN!'
+  var Pin='Enter enter your new PIN!'
+  var cpin='Enter the Confirm PIN'
   var compare='Your new PIN and confirm PIN do not match'
   var len='PIN must be 4 digit'
   var text=""
   var showSnack=true
+  var list=[]
+  var pin=this.state.pin
+  for(var i in pin){
+      list.push(parseInt(pin[i]))
+  }
+  var one=list[0]+3
+  var two=list[1]+2
+  var three=list[2]+1
+
+  var rone=list[0]-3
+  var rtwo=list[1]-2
+  var rthree=list[2]-1
+  
   if(!this.state.pin){
-      text=pin
-  }else if(!this.state.confirm){
+    text=Pin
+}else if(!this.state.confirm){
       text=cpin
   }else if(this.state.pin.lengh<4 || this.state.confirm.length<4){
       text=len
   }
   else if(this.state.pin!=this.state.confirm){
       text=compare
-  }else{
+  }else if(one==two && two == three && three==list[3]){
+      text='Pin should not be sequential in ascending order'
+  }
+  else if(rone==rtwo && rtwo == rthree && rthree==list[3]){
+      text='Pin should not be sequential in descending order'
+  }
+  else if((list[0]==list[1] && list[1]==list[2])|| (list[1]==list[2] && list[2]==list[3])){
+      text="PIN cannot contain same digit consecutively more than 2 times"
+  }
+  else{
       this.props.navigation.navigate('Photo')
       showSnack=false
   }
@@ -127,3 +149,42 @@ const styles=StyleSheet.create({
 
             
                 
+
+
+
+
+
+
+
+
+// handleNext=()=>{
+//   var pin='Enter enter your new PIN!'
+//   var cpin='Please re-enter your new PIN!'
+//   var compare='Your new PIN and confirm PIN do not match'
+//   var len='PIN must be 4 digit'
+//   var text=""
+//   var showSnack=true
+//   if(!this.state.pin){
+//       text=pin
+//   }else if(!this.state.confirm){
+//       text=cpin
+//   }else if(this.state.pin.lengh<4 || this.state.confirm.length<4){
+//       text=len
+//   }
+//   else if(this.state.pin!=this.state.confirm){
+//       text=compare
+//   }else{
+//       this.props.navigation.navigate('Photo')
+//       showSnack=false
+//   }
+//   if(showSnack){
+//      SnackBar.show({
+//          text:text,
+//          duration:SnackBar.LENGTH_LONG,
+//          action:{
+//              text:'OK',
+//              textColor:'red'
+//          }
+//      }) 
+//   }
+// }
